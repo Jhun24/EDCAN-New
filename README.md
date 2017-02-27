725c218d3e14b25673430ed1baa934b8c590b574
Jhun24 <jjanghuni102@naver.com>
2017-02-27 18:49:37 +0900

    add js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # EDCAN-New
 
 > 나눔고딕 : font-family: 'Nanum Barun Gothic', sans-serif;
+
+> Geometos : font-family:"Geometos",sans-serif;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 /**
  * Created by janghunlee on 2017. 2. 27..
  */
+
+$(".footer").hover(
+    function(){
+
+        $(this).css({
+            "backgroundColor":"rgba(0,0,0,0.5)"
+        })
+    },
+    function(){
+        $(this).css({
+            "background-color":"transparent"
+        })
+    }
+)
