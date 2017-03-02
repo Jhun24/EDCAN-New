42677532c8991a6ce97b82ba8ff606742b4f8c1e
Jhun24 <jjanghuni102@naver.com>
2017-03-02 23:21:17 +0900

    sibalsldlfasdlf

---
+++ js/access.js
@@ -0,0 +1,11 @@
+/**
+ * Created by janghunlee on 2017. 3. 2..
+ */
+
+$(".edcan").click(function(){
+    location.href="https://edcan.kr"
+})
+
+$(".back").click(function(){
+    history.back()
+})
